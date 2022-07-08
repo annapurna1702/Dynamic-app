fe2cb0557480ab9d75b867f8547a20cd939e88c0
annapurna1702 <annapurnapadmaprema@gmail.com>
2022-07-08 15:45:53 +0530

    Student details

--- src/app/viewstudents/viewstudents.component.ts
+++ src/app/viewstudents/viewstudents.component.ts
@@ -16,7 +16,7 @@ export class ViewstudentsComponent implements OnInit {
   {"name":"Abhishek Prem","Age":20,"photo":"https://365webresources.com/wp-content/uploads/2016/09/FREE-PROFILE-AVATARS.png"},
   {"name":"Kiara","Age":20,"photo":"https://tse3.mm.bing.net/th?id=OIP.C2mTGdKXdgQ12X0usocvRgHaHa&pid=Api&P=0&w=156&h=156"},
   {"name":"Mohan","Age":35,"photo":"https://365webresources.com/wp-content/uploads/2016/09/FREE-PROFILE-AVATARS.png"},
-  {"name":"Radha","Age":25,"photo":"https://tse3.mm.bing.net/th?id=OIP.C2mTGdKXdgQ12X0usocvRgHaHa&pid=Api&P=0&w=156&h=156"},
+  {"name":"Radha","Age":25,"photo":"https://tse3.mm.bing.net/th?id=OIP.C2mTGdKXdgQ12X0usocvRgHaHa&pid=Api&P=0&w=156&h=156"}
 
 
 
